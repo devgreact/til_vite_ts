982b0cebeadb6b8796ea6e683bbca413b6ea3e25
devgreact <dev.greact@gmail.com>
2025-09-10 13:30:00 +0900

    up

--- src/pages/TodosPage.tsx
+++ src/pages/TodosPage.tsx
@@ -1,15 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { TodoProvider } from '../contexts/TodoContext';
+import { TodoProvider, useTodos } from '../contexts/TodoContext';
 import TodoWrite from '../components/todos/TodoWrite';
 import TodoList from '../components/todos/TodoList';
 import type { Profile } from '../types/TodoType';
 import { useAuth } from '../contexts/AuthContext';
 import { getProfile } from '../lib/profile';
+import Pagination from '../components/Pagination';
+
+// 용서하세요. 컴포넌트는 여기서 작성하겠습니다.
+// 필요하시면 이동 부탁합니다.
+interface TodosContentProps {
+  currentPage: number;
+  itemsPerPage: number;
+  handleChangePage: (page: number) => void;
+}
+const TodosContent = ({
+  currentPage,
+  itemsPerPage,
+  handleChangePage,
+}: TodosContentProps): JSX.Element => {
+  const { totalCount, totalPages } = useTodos();
+  return (
+    <div>
+      <div>
+        <TodoWrite />
+      </div>
+      <div>
+        <TodoList />
+      </div>
+      <div>
+        <Pagination
+          totalCount={totalCount}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          itemsPerPage={itemsPerPage}
+          handleChangePage={handleChangePage}
+        />
+      </div>
+    </div>
+  );
+};
 
 function TodosPage() {
   const { user } = useAuth();
-  const [profile, setProfile] = useState<Profile | null>(null);
 
+  // 페이지네이션 관련
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  // const itemsPerPage = 10;
+  // 페이지 변경 핸들러
+  const handleChangePage = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const [profile, setProfile] = useState<Profile | null>(null);
   // 프로필 가져오기
   const loadProfile = async () => {
     try {
@@ -33,12 +77,11 @@ function TodosPage() {
     <div>
       <h2>{profile?.nickname}할 일</h2>
       <TodoProvider>
-        <div>
-          <TodoWrite />
-        </div>
-        <div>
-          <TodoList />
-        </div>
+        <TodosContent
+          currentPage={currentPage}
+          itemsPerPage={itemsPerPage}
+          handleChangePage={handleChangePage}
+        />
       </TodoProvider>
     </div>
   );
